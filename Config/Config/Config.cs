205e2acd6f7653a190675a7c05738a24c887e850
Premek Vysoky <premek.vysoky@gmail.com>
2016-03-24 10:49:21 +0100

    Retarget project to .NET v4.5

--- Config/Config/Config.cs
+++ Config/Config/Config.cs
@@ -2,5 +2,10 @@
 {
     public class Config : IConfig
     {
+	    public ConfigSection this[string key]
+	    {
+		    get { throw new System.NotImplementedException(); }
+		    set { throw new System.NotImplementedException(); }
+	    }
     }
 }
