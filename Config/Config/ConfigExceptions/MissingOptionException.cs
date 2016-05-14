444ac7c716d1b0ef6f443c8e85d8cdb635f64550
wolen <vavrekmichal@gmail.com>
2016-05-15 00:39:37 +0200

    Todo test redundant members

--- Config/Config/ConfigExceptions/MissingOptionException.cs
+++ Config/Config/ConfigExceptions/MissingOptionException.cs
@@ -9,13 +9,5 @@ namespace Config.ConfigExceptions
 		{
 		}
 	}
-
-    public class RedundantOptionException : ConfigException
-    {
-        public RedundantOptionException(string sectionName, string optionName)
-            : base("Not specified option '" + sectionName + "#" + optionName + "' found")
-        {
-        }
-    }
 }
 
